380a8bf0bf01f70cc1176d3e28f8f10ab91b204f
Robin Tabard <rpctab@gmail.com>
2019-05-27 14:19:04 +0200

    fix du fichier de migration pour ajouter la valeur par défaut

--- src/Migrations/Version20190527121211.php
+++ src/Migrations/Version20190527121211.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190527080134 extends AbstractMigration
+final class Version20190527121211 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20190527080134 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD is_active TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE is_active is_active TINYINT(1) DEFAULT \'1\' NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20190527080134 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user DROP is_active');
+        $this->addSql('ALTER TABLE user CHANGE is_active is_active TINYINT(1) NOT NULL');
     }
 }
