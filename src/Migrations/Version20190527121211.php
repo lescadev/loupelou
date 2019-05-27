9412bc3f96ae3a7f395e9c7dc0fafad2e3173534
Robin Tabard <rpctab@gmail.com>
2019-05-27 15:56:08 +0200

    fix du fichier de migration V3

--- src/Migrations/Version20190527121211.php
+++ src/Migrations/Version20190527121211.php
@@ -22,7 +22,7 @@ final class Version20190527121211 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD is_active is_active TINYINT(1) DEFAULT \'1\' NOT NULL');
+        $this->addSql('ALTER TABLE user ADD is_active TINYINT(1) DEFAULT \'1\' NOT NULL');
     }
 
     public function down(Schema $schema) : void
