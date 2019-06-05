79566cd2d9133e932ac57b1fecc354185342058f
MathieuCornut <mathieu.cornut.tic@gmail.com>
2019-06-05 11:49:46 +0200

    test slogan 0.1

---
+++ src/Service/Slogan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Service;
+use App\Repository\InformationsRepository;
+
+class Slogan {
+
+    public function getSlogan(){
+        //$info = $informationsrepository->find(1);
+        $info = "test";
+        return $info;
+    }
+}
+
