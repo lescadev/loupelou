de4b3451cbc3ab89d08443aa0d8699ceb629b04c
theo <theo.lanique@gmail.com>
2019-07-20 13:36:01 +0200

    Update and remove no more used bundle
    Useless config file remove
    
    Useless config file

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
