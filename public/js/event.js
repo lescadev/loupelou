27f544facc34b75800d653f9d5942092a821d041
Mihidari <theo.lanique@gmail.com>
2019-07-24 11:48:54 +0200

    Ajax modal pour les prestataires

--- public/js/event.js
+++ public/js/event.js
@@ -22,3 +22,41 @@ function setEvent(status) {
     sendData(status)
 }
 
+function setEventModal() {
+
+    let cards = document.getElementsByClassName('card');
+    let modalPresta = document.getElementById('modal_presta');
+
+    if (cards && modalPresta) {
+        for (let card of cards) {
+            card.addEventListener("click", function () {
+                $("#modal_presta").modal('show');
+                ajaxModal(card.id);
+            })
+        }
+    }
+}
+
+function setModalContent(data) {
+    //Visiblement y'a JQuery j'étais pas au courant
+
+    let denomination = $('#presta_denomination');
+    let categorie = $('#categorie');
+    let description = $('#description');
+    let site = $('#site');
+    let adresse = $('#adresse');
+
+    denomination.empty();
+    categorie.empty();
+    description.empty();
+    site.empty();
+    adresse.empty();
+
+    denomination.append(data.nom);
+    categorie.append(data.categorie);
+    description.append(data.description);
+    site.append(data.site);
+    adresse.append(data.rue + "<br>" + data.code_postal + "<br>" + data.ville);
+
+
+}
