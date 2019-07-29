db5d19cb1fa8ed093ef1789f26ae76e9d7ed910d
Thomas <thomasdufayet3@gmail.com>
2019-07-29 10:48:50 +0200

    fix migration

--- src/Migrations/Version20190729083836.php
+++ src/Migrations/Version20190729083836.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190722143653 extends AbstractMigration
+final class Version20190729083836 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,6 +22,9 @@ final class Version20190722143653 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE transaction (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, comptoir_id INT NOT NULL, montant DOUBLE PRECISION NOT NULL, date_transaction DATETIME NOT NULL, INDEX IDX_723705D1A76ED395 (user_id), INDEX IDX_723705D1AEB0C1F5 (comptoir_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D1A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D1AEB0C1F5 FOREIGN KEY (comptoir_id) REFERENCES comptoir (id)');
         $this->addSql('ALTER TABLE comptoir ADD solde DOUBLE PRECISION DEFAULT NULL');
     }
 
@@ -30,6 +33,7 @@ final class Version20190722143653 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('DROP TABLE transaction');
         $this->addSql('ALTER TABLE comptoir DROP solde');
     }
 }
