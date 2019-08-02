9cb1420bde2779432d91b591336fe56cdcdfc924
Mihidari <theo.lanique@gmail.com>
2019-08-02 15:08:27 +0200

    Fix version

--- src/Controller/EasyAdminCustomController.php
+++ src/Controller/EasyAdminCustomController.php
@@ -4,39 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Comptoir;
 use App\Entity\Prestataire;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
-use EasyCorp\Bundle\EasyAdminBundle\Configuration\ConfigManager;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminControllerTrait;
-use EasyCorp\Bundle\EasyAdminBundle\Form\Filter\FilterRegistry;
-use EasyCorp\Bundle\EasyAdminBundle\Search\Autocomplete;
-use EasyCorp\Bundle\EasyAdminBundle\Search\Paginator;
-use EasyCorp\Bundle\EasyAdminBundle\Search\QueryBuilder;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
 
-
-class EasyAdminCustomController extends AbstractController {
-
-    use AdminControllerTrait;
-
-    /** @var EntityManager|null The Doctrine entity manager for the current entity */
-    protected $em;
-
-    public static function getSubscribedServices(): array
-    {
-        return parent::getSubscribedServices() + [
-                'easyadmin.autocomplete' => Autocomplete::class,
-                'easyadmin.config.manager' => ConfigManager::class,
-                'easyadmin.paginator' => Paginator::class,
-                'easyadmin.query_builder' => QueryBuilder::class,
-                'easyadmin.property_accessor' => PropertyAccessorInterface::class,
-                'easyadmin.filter.registry' => FilterRegistry::class,
-                'event_dispatcher' => EventDispatcherInterface::class,
-            ];
-    }
+class EasyAdminCustomController extends EasyAdminController {
 
     /**
      * Delete Prestataire Entity AND associated user if not linked to another comptoir
